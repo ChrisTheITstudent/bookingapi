adf5fa19cbf8e1fb3448d757cdee1f8e04b35b41
ChrisTheITstudent <S3965755@student.rmit.edu.au>
2024-04-03 20:40:36 +1000

    Contact Table fix
    typo fixed

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class Contact(BaseModel):
     phoneNo: str | None = None
     email: str | None = None
 
+class Booking(BaseModel):
+    contactID: int | None = None
+
 db = mysql.connector.connect(
     host="908.h.filess.io",
     port="3307",
@@ -23,7 +26,7 @@ app = FastAPI()
 # Create a contact
 @app.post("/contacts/")
 def create_contact(contact: Contact):
-    sql = "INSERT INTO contacts (contactName, phoneNo, email) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO Contact (contactName, phoneNo, email) VALUES (%s, %s, %s)"
     val =  (contact.name, contact.phoneNo, contact.email)
     cursor.execute(sql, val)
     db.commit()
@@ -32,7 +35,7 @@ def create_contact(contact: Contact):
 # Update a contact
 @app.put("/contacts/{contact_id}")
 def update_contact(contact_id: int, contact: Contact):
-    sql = "UPDATE contacts SET contactName = %s, phoneNo = %s, email = %s WHERE contactID = %s"
+    sql = "UPDATE Contact SET contactName = %s, phoneNo = %s, email = %s WHERE contactID = %s"
     val =  (contact.name, contact.phoneNo, contact.email, contact_id)
     cursor.execute(sql, val)
     db.commit()
@@ -41,22 +44,54 @@ def update_contact(contact_id: int, contact: Contact):
 # Read contacts operations
 @app.get("/contacts/")
 def read_contacts():
-    cursor.execute("SELECT * FROM contacts")
+    cursor.execute("SELECT * FROM Contact")
     contacts = cursor.fetchall()
     return contacts
 # Read a contact
 @app.get("/contacts/{contact_id}")
 def read_contact(contact_id: int):
-    cursor.execute("SELECT * FROM contacts WHERE contactID = %s", (contact_id,))
+    cursor.execute("SELECT * FROM Contact WHERE contactID = %s", (contact_id,))
     contact = cursor.fetchone()
     return contact
 
 # Delete a contact
-@app.delete("/contacts/{contact_id}")
+@app.delete("/contacts/delete/{contact_id}")
 def delete_contact(contact_id: int):
-    cursor.execute("DELETE FROM contacts WHERE contactID = %s", (contact_id,))
+    cursor.execute("DELETE FROM Contact WHERE contactID = %s", (contact_id,))
     db.commit()
     return {"message": "Contact deleted successfully"}
+
+# ---------------------------End of Contacts end points--------------------------
+# ---------------------------Bookings end points---------------------------------
+# Create a booking
+@app.post("/bookings/")
+def create_booking(booking: Booking):
+    sql = "INSERT INTO bookings (contactID) VALUES (%s)"
+    val =  (booking.contactID,)
+    cursor.execute(sql, val)
+    db.commit()
+    return {"contactID": booking.contactID}
+
+# Read bookings operations
+@app.get("/bookings/")
+def read_bookings():
+    cursor.execute("SELECT * FROM bookings")
+    bookings = cursor.fetchall()
+    return bookings
+
+# Read a booking
+@app.get("/bookings/{booking_id}")
+def read_booking(booking_id: int):
+    cursor.execute("SELECT * FROM bookings WHERE bookingID = %s", (booking_id,))
+    booking = cursor.fetchone()
+    return booking
+
+# Delete a booking
+@app.delete("/bookings/delete/{booking_id}")
+def delete_booking(booking_id: int):
+    cursor.execute("DELETE FROM bookings WHERE bookingID = %s", (booking_id,))
+    db.commit()
+    return {"message": "Booking deleted successfully"}
 
 # Root end point
 @app.get("/")
